8dc841e5ef8da17951d5f2801e6f866111f2ea06
won <andrew112517@naver.com>
2025-03-26 00:52:04 +0900

    [feat/#6] 로그인 화면 경로 추가

--- CUKBOB-iOS/Sources/Global/Navigation/FullScreenModalDestination.swift
+++ CUKBOB-iOS/Sources/Global/Navigation/FullScreenModalDestination.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 public enum FullScreenModalDestination: Hashable, Identifiable {
-    case temp
+    case login
     
     public var id: UUID {
         switch self {
-        case .temp: return UUID()
+        case .login: return UUID()
         }
     }
     
     @ViewBuilder
     func build() -> some View {
         switch self {
-        case .temp:
-            TempView()
+        case .login:
+            LoginView()
         }
     }
 }
