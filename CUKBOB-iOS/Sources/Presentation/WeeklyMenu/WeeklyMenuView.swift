598abd62b2f2962359f90bbf5ba8ba2ce4381c9d
Won <andrew112517@naver.com>
2025-05-20 15:26:20 +0900

    [refactor] 주간 캘린더 수정

--- CUKBOB-iOS/Sources/Presentation/WeeklyMenu/WeeklyMenuView.swift
+++ CUKBOB-iOS/Sources/Presentation/WeeklyMenu/WeeklyMenuView.swift
@@ -16,17 +16,15 @@ struct WeeklyMenuView: View {
     // MARK: - body
     
     var body: some View {
-        VStack(alignment: .center, spacing: Screen.height(0)) {
+        VStack(alignment: .center, spacing: 0) {
             horizontalCalendarSection
             
             Spacer()
         }
         .background {
             Color(.blue100)
-                .onTapGesture {
-                    hideKeyboard()
-                }
         }
+        .clipped()
     }
 }
 
@@ -34,41 +32,38 @@ struct WeeklyMenuView: View {
 
 private extension WeeklyMenuView {
     var horizontalCalendarSection: some View {
-        GeometryReader { geometry in
-            VStack(alignment: .leading, spacing: Screen.height(24)) {
-                CUKBOBText(viewModel.currentMonthString(), fontType: .label01, color: Color(.blue0))
-                    .padding(.horizontal, Screen.width(12))
-                    .padding(.vertical, Screen.height(4))
-                    .background(Color(.blue600))
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                
-                HStack(alignment: .top, spacing: 0) {
-                    ForEach (0..<7) { index in
-                        let calenderDate = viewModel.currentWeekDates()[index]
-                        if let calendarWeekDay = Weekday(rawValue: index) {
-                            CalendarDayButton(
-                                viewModel: viewModel,
-                                weekDay: calendarWeekDay,
-                                date: calenderDate
-                            ) {
-                                print("선택된 날짜: \(viewModel.selectedDate.dateString)")
-                            }
+        VStack(alignment: .leading, spacing: Screen.height(24)) {
+            CUKBOBText(viewModel.currentMonthString(), fontType: .label01, color: Color(.blue0))
+                .padding(.horizontal, Screen.width(12))
+                .padding(.vertical, Screen.height(4))
+                .background(Color(.blue600))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            HStack(alignment: .top, spacing: 0) {
+                ForEach (0..<7) { index in
+                    let calenderDate = viewModel.currentWeekDates()[index]
+                    if let calendarWeekDay = Weekday(rawValue: index) {
+                        CalendarDayButton(
+                            viewModel: viewModel,
+                            weekDay: calendarWeekDay,
+                            date: calenderDate
+                        ) {
+                            print("선택된 날짜: \(viewModel.selectedDate.dateString)")
                         }
-                        
-                        if index != 6 {
-                            Spacer()
-                        }
+                    }
+                    
+                    if index != 6 {
+                        Spacer()
                     }
                 }
             }
-            .padding(.top, Screen.height(14 + geometry.safeAreaInsets.top))
-            .padding(.bottom, Screen.height(15))
-            .padding(.horizontal, Screen.width(41))
-            .background(Color(.gray0))
-            .cornerRadius(24, corners: [.bottomLeft, .bottomRight])
-            .shadow(color: Color(.blue200), radius: 10)
-            .ignoresSafeArea()
         }
+        .padding(.top, Screen.height(14))
+        .padding(.bottom, Screen.height(15))
+        .padding(.horizontal, Screen.width(41))
+        .background(Color(.gray0))
+        .cornerRadius(24, corners: [.bottomLeft, .bottomRight])
+        .shadow(color: Color(.blue200), radius: 10)
     }
 }
 
