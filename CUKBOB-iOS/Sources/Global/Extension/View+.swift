ca0cd421d94fb722f0e2ce677a11fee7a681dcdd
Won <andrew112517@naver.com>
2025-06-02 19:47:01 +0900

    [feat/#29] CustomNavigationBar 구현

--- CUKBOB-iOS/Sources/Global/Extension/View+.swift
+++ CUKBOB-iOS/Sources/Global/Extension/View+.swift
@@ -21,3 +21,86 @@ extension View {
         background(ViewDidAppearModifier(onDidAppear: perform))
     }
 }
+
+// MARK: - Custom NavigationBar
+
+extension View {
+    @ViewBuilder
+    func customNavigationBar(_ navigationBarType: NavigationBarType) -> some View {
+        switch navigationBarType {
+        case .seeReview(let backAction):
+            self.modifier(
+                CustomNavigationBarModifier(
+                    centerView: {
+                        CUKBOBText("리뷰", fontType: .heading02, color: Color(.blue700))
+                    },
+                    leftView: {
+                        Button {
+                            backAction()
+                        } label: {
+                            Image(.arrowLeft)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: Screen.width(18), height: Screen.height(18))
+                        }
+                    },
+                    rightView: {
+                        EmptyView()
+                    },
+                    backgroundColor: Color(.blue100)
+                )
+            )
+        case .writeReview(let backAction):
+            self.modifier(
+                CustomNavigationBarModifier(
+                    centerView: {
+                        CUKBOBText("리뷰 작성", fontType: .heading02, color: Color(.blue700))
+                    },
+                    leftView: {
+                        Button {
+                            backAction()
+                        } label: {
+                            Image(.arrowLeft)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: Screen.width(18), height: Screen.height(18))
+                        }
+                    },
+                    rightView: {
+                        EmptyView()
+                    },
+                    backgroundColor: Color(.blue100)
+                )
+            )
+        case .foodAndBeverageDetail(let backAction):
+            self.modifier(
+                CustomNavigationBarModifier(
+                    centerView: {
+                        EmptyView()
+                    },
+                    leftView: {
+                        Button {
+                            backAction()
+                        } label: {
+                            Image(.arrowLeft)
+                                .renderingMode(.template)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: Screen.width(24), height: Screen.height(24))
+                                .foregroundStyle(Color(.gray0))
+                        }
+                    },
+                    rightView: {
+                        EmptyView()
+                    }
+                )
+            )
+        }
+    }
+}
+
+enum NavigationBarType {
+    case seeReview(backAction: () -> Void)
+    case writeReview(backAction: () -> Void)
+    case foodAndBeverageDetail(backAction: () -> Void)
+}
